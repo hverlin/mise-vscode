896ba31120cc66d6883759ee693dfc3fe37c8be4
hverlin <hugues.verlin@gmail.com>
2025-10-07 13:03:26 +0200

    doc: update deps

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,7 +2,6 @@ import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 import starlightImageZoom from "starlight-image-zoom";
 import starlightLinksValidator from "starlight-links-validator";
-import starlightThemeObsidian from "starlight-theme-obsidian";
 
 export default defineConfig({
 	site: "https://hverlin.github.io/mise-vscode/",
@@ -18,17 +17,18 @@ export default defineConfig({
 					exclude: ["http://127.0.0.1:8000/"],
 				}),
 				starlightImageZoom(),
-				starlightThemeObsidian({
-					graphConfig: { visibilityRules: [] },
-				}),
 			],
 			customCss: ["./src/styles/global.css"],
 			editLink: {
 				baseUrl: "https://github.com/hverlin/mise-vscode/tree/main/docs/",
 			},
-			social: {
-				github: "https://github.com/hverlin/mise-vscode",
-			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/hverlin/mise-vscode",
+				},
+			],
 		}),
 	],
 });
