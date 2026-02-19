10a92fa5f50cec120d97ea6d24c237531d075f40
Ranieri Valença <ranierivalenca@gmail.com>
2026-02-18 23:50:53 -0300

    feat: enhance MiseTomlTaskSymbolProvider to support additional sections and improve symbol creation

--- src/providers/MiseTomlTaskSymbolProvider.test.ts
+++ src/providers/MiseTomlTaskSymbolProvider.test.ts
@@ -13,8 +13,6 @@ class MockDocumentSymbol {
 }
 
 const MockSymbolKind = {
-    Function: 1,
-    Property: 2,
 };
 
 const provider = new MiseTomlTaskSymbolProvider({
@@ -29,64 +27,103 @@ describe("MiseTomlTaskSymbolProvider tests", () => {
         const document = {
             getText: () =>
                 `
+[tools]
+key_tool = "value_tool"
+
+[env]
+env_key = "value_env"
+env_key2 = "value_env2"
+
+[settings]
+setting_key = "value_setting"
+
 [tasks]
 key1 = "value1"
 "key2"= "value2"
-"complex \"key\" 3" = "value3"
+"complex \\"key\\" 3" = "value3"
 
 [tasks.example]
 key_example = "value4"
 
-[tasks."complex [\"section\"] name"]
+[tasks."complex [\\"section\\"] name"]
 key_complex = "value5"
 
 [other]
 key_other = "value6"
 `.trim(),
         } as vscode.TextDocument;
+        const toolsSectionLength = "[tools]".length;
+        const envSectionLength = "[env]".length;
+        const settinsSectionLength = "[settings]".length;
+
+        const key1Length = 'key1 = "value1"'.length;
+        const key2Length = '"key2"= "value2"'.length;
+        const complexKey3Length = '"complex \\"key\\" 3" = "value3"'.length;
+
+        const exampleSectionLength = "[tasks.example]".length;
+        const complexSectionLength = '[tasks."complex [\\"section\\"] name"]'.length;
         
-        console.log("vscode resolve:", require.resolve("vscode"));
-console.log("vscode version:", (vscode as any).version);
         const symbols = provider.provideDocumentSymbols(document, {} as vscode.CancellationToken) as MockDocumentSymbol[];
 
         expect(symbols).toEqual([
+            new MockDocumentSymbol(
+                "[tools]",
+                '',
+                undefined as any,
+                new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, toolsSectionLength)),
+                new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, toolsSectionLength)),
+            ),
             // only trivial key-value pairs should be included as symbols from "tasks" section
             new MockDocumentSymbol(
+                "[env]",
+                '',
+                undefined as any,
+                new vscode.Range(new vscode.Position(3, 0), new vscode.Position(3, envSectionLength)),
+                new vscode.Range(new vscode.Position(3, 0), new vscode.Position(3, envSectionLength)),
+            ),
+            new MockDocumentSymbol(
+                "[settings]",
+                '',
+                undefined as any,
+                new vscode.Range(new vscode.Position(7, 0), new vscode.Position(7, settinsSectionLength)),
+                new vscode.Range(new vscode.Position(7, 0), new vscode.Position(7, settinsSectionLength)),
+            ),
+            new MockDocumentSymbol(
                 "key1",
-                '"value1"',
-                MockSymbolKind.Property,
-                new vscode.Range(new vscode.Position(1, 0), new vscode.Position(1, 15)),
-                new vscode.Range(new vscode.Position(1, 0), new vscode.Position(1, 15)),
+                'value1',
+                undefined as any,
+                new vscode.Range(new vscode.Position(11, 0), new vscode.Position(11, key1Length)),
+                new vscode.Range(new vscode.Position(11, 0), new vscode.Position(11, key1Length)),
             ),
             new MockDocumentSymbol(
                 "key2",
-                '"value2"',
-                MockSymbolKind.Property,
-                new vscode.Range(new vscode.Position(2, 0), new vscode.Position(2, 16)),
-                new vscode.Range(new vscode.Position(2, 0), new vscode.Position(2, 16)),
+                'value2',
+                undefined as any,
+                new vscode.Range(new vscode.Position(12, 0), new vscode.Position(12, key2Length)),
+                new vscode.Range(new vscode.Position(12, 0), new vscode.Position(12, key2Length)),
             ),
             new MockDocumentSymbol(
                 'complex "key" 3',
-                '"value3"',
-                MockSymbolKind.Property,
-                new vscode.Range(new vscode.Position(3, 0), new vscode.Position(3, 28)),
-                new vscode.Range(new vscode.Position(3, 0), new vscode.Position(3, 28)),
+                'value3',
+                undefined as any,
+                new vscode.Range(new vscode.Position(13, 0), new vscode.Position(13, complexKey3Length)),
+                new vscode.Range(new vscode.Position(13, 0), new vscode.Position(13, complexKey3Length)),
             ),
 
             // "example" and "complex [\"section\"] name" sections should be included as symbols
             new MockDocumentSymbol(
                 "example",
                 "",
-                MockSymbolKind.Function,
-                new vscode.Range(new vscode.Position(5, 0), new vscode.Position(5, 15)),
-                new vscode.Range(new vscode.Position(5, 0), new vscode.Position(5, 15)),
+                undefined as any,
+                new vscode.Range(new vscode.Position(15, 0), new vscode.Position(15, exampleSectionLength)),
+                new vscode.Range(new vscode.Position(15, 0), new vscode.Position(15, exampleSectionLength)),
             ),
             new MockDocumentSymbol(
                 'complex ["section"] name',
                 '',
-                MockSymbolKind.Function,
-                new vscode.Range(new vscode.Position(8, 0), new vscode.Position(8, 34)),
-                new vscode.Range(new vscode.Position(8, 0), new vscode.Position(8, 34)),
+                undefined as any,
+                new vscode.Range(new vscode.Position(18, 0), new vscode.Position(18, complexSectionLength)),
+                new vscode.Range(new vscode.Position(18, 0), new vscode.Position(18, complexSectionLength)),
             ),
 
             // "other" section should be ignored, as it's not a "tasks" section
--- src/providers/MiseTomlTaskSymbolProvider.ts
+++ src/providers/MiseTomlTaskSymbolProvider.ts
@@ -1,44 +1,71 @@
 import * as vscode from "vscode";
+import { MiseTomlType, TomlParser } from "../utils/miseFileParser";
 
 type VscodeLike = Pick<
   typeof vscode,
   "SymbolKind" | "DocumentSymbol" | "Range" | "Position"
 >;
-
 export class MiseTomlTaskSymbolProvider
   implements vscode.DocumentSymbolProvider
 {
   private readonly TRIVIAL_TASK_SYMBOL_KIND;
   private readonly TASK_SECTION_SYMBOL_KIND;
+  private readonly SECTION_SYMBOL_KIND;
 
   constructor(private readonly vscode: VscodeLike) {
     this.vscode = vscode;
     this.TRIVIAL_TASK_SYMBOL_KIND = vscode.SymbolKind.Property;
     this.TASK_SECTION_SYMBOL_KIND = vscode.SymbolKind.Function;
+    this.SECTION_SYMBOL_KIND = vscode.SymbolKind.Namespace;
   }
 
-  private createDocumentSymbol(
+  private createSymbol(
     name: string,
     detail: string,
     kind: vscode.SymbolKind,
     range: vscode.Range,
-    selectionRange: vscode.Range,
   ): vscode.DocumentSymbol {
-    return new this.vscode.DocumentSymbol(
-      name,
-      detail,
-      kind,
-      range,
-      selectionRange,
-    );
+    const unquotedName = name.replace(/^"(.*)"$/, "$1");
+    return new this.vscode.DocumentSymbol(name, detail, kind, range, range);
   }
 
-  private createRange(
-    startLine: number,
-    startCharacter: number,
-    endLine: number,
-    endCharacter: number,
-  ): vscode.Range {
+  private getKeyData(
+    entries: Record<string, any>,
+    sourceTracker: TomlParser<MiseTomlType>["sourceTracker"],
+    lines: string[],
+    key: string,
+  ): {
+    name: string;
+    detail: string;
+    range: vscode.Range;
+  } {
+    const { start, file } = sourceTracker.getKeySource(entries, key);
+    const lineNumber = file.lineNumberAt(start) - 1; // lineNumberAt is 1-based, we need 0-based
+    const line = lines[lineNumber];
+    const ret = {
+      name: key,
+      detail: typeof entries[key] === "string" ? entries[key] : "",
+      range: new this.vscode.Range(
+        new this.vscode.Position(0, 0),
+        new this.vscode.Position(0, 0),
+      ),
+    };
+    if (!line) {
+      return ret;
+    }
+    const range = this.createRange(lineNumber, line);
+    return {
+      ...ret,
+      range,
+    };
+  }
+
+  private createRange(index: number, line: string): vscode.Range {
+    const startLine = index;
+    const endLine = index;
+    const startCharacter = 0;
+    const endCharacter = line.length;
+
     return new vscode.Range(
       new this.vscode.Position(startLine, startCharacter),
       new this.vscode.Position(endLine, endCharacter),
@@ -50,66 +77,51 @@ export class MiseTomlTaskSymbolProvider
     _token: vscode.CancellationToken,
   ): vscode.ProviderResult<vscode.DocumentSymbol[]> {
     const symbols: vscode.DocumentSymbol[] = [];
-    const text = document.getText();
-    const lines = text.split(/\r?\n/);
 
-    const sectionRegex = /^\s*\[\s*tasks(?:\.(.+?))?\s*\]\s*$/;
+    const { parsed, sourceTracker } = new TomlParser<
+      MiseTomlType & { [key: string]: Record<string, any> }
+    >(document.getText());
+    const lines = document.getText().split(/\r?\n/);
 
-    const trivialRegex =
-      /^\s*(?:"(.+)"\s*=|(?:([A-Za-z0-9_-]+)\s*=))\s*(.+)\s*$/;
-
-    let checkingTasksSection = false;
-    lines.forEach((line, index) => {
-      const lineRange = this.createRange(index, 0, index, line.length);
-      const sectionMatch = line.match(sectionRegex);
-      if (sectionMatch) {
-        const rawName = sectionMatch[1];
-        const sectionName = rawName || "tasks";
-
-        // it is just the "tasks" section, we set checkingTasksSection to true,
-        // so we can check for trivial key-value pairs in the following lines,
-        // until we find another section
-        if (rawName === undefined) {
-          checkingTasksSection = true;
-          return;
-        }
-        if (checkingTasksSection && rawName !== undefined) {
-          // we found a subsection of "tasks", we can stop checking for trivial key-value pairs
-          checkingTasksSection = false;
-        }
-
-        const unquotedSectionName = sectionName.replace(/^"(.*)"$/, "$1");
-
-        const sectionSymbol = this.createDocumentSymbol(
-          unquotedSectionName,
-          "",
-          this.TASK_SECTION_SYMBOL_KIND,
-          lineRange,
-          lineRange,
+    for (const mainKey in parsed) {
+      if (["settings", "env", "tools"].includes(mainKey)) {
+        const { name, detail, range } = this.getKeyData(
+          parsed,
+          sourceTracker,
+          lines,
+          mainKey,
         );
-        symbols.push(sectionSymbol);
-        return;
-      }
-      if (!checkingTasksSection) {
-        return;
-      }
-      const trivialMatch = line.match(trivialRegex);
-      if (trivialMatch) {
-        const key = trivialMatch[1] || trivialMatch[2];
-        const value = trivialMatch[3];
-        if (key === undefined || value === undefined) {
-          return;
-        }
-        const trivialSymbol = this.createDocumentSymbol(
-          key,
-          value,
-          this.TRIVIAL_TASK_SYMBOL_KIND,
-          lineRange,
-          lineRange,
+        symbols.push(
+          this.createSymbol(
+            `[${name}]`,
+            detail,
+            this.SECTION_SYMBOL_KIND,
+            range,
+          ),
         );
-        symbols.push(trivialSymbol);
       }
-    });
+      if (mainKey === "tasks") {
+        for (const key in parsed.tasks) {
+          const value = parsed.tasks[key];
+          const { name, detail, range } = this.getKeyData(
+            parsed.tasks,
+            sourceTracker,
+            lines,
+            key,
+          );
+          symbols.push(
+            this.createSymbol(
+              name,
+              typeof value === "string" ? detail : "",
+              typeof value === "string"
+                ? this.TRIVIAL_TASK_SYMBOL_KIND
+                : this.TASK_SECTION_SYMBOL_KIND,
+              range,
+            ),
+          );
+        }
+      }
+    }
     return symbols;
   }
 }
